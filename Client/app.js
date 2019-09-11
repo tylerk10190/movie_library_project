1895fa19ee534dc69064879346d6ea4b92f9e428
samoski615 <samoski615@gmail.com>
2019-09-11 10:50:24 -0500

    committing changes before pulling from main branch

--- Client/app.js
+++ Client/app.js
@@ -1,8 +1,49 @@
+// POST action -- add new movie to list //
 (function($){
     function processForm( e ){
         var dict = {
         	Title : this["title"].value,
-        	Director: this["director"].value
+        	Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44352/api/movie',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#my-form').submit( processForm );
+})(jQuery);
+
+// get details of one movie: GET //
+// function GetMovie(value) {
+//     console.log(value);
+//     $.ajax({
+//         url: 'https://localhost:44352/api//movie/GET',
+//         context : document.body
+//     }).done(function(){
+//         $(this).addClass("done");
+//     });
+// }
+
+(function($){
+    function processForm( e ){
+        var dict = {
+            Title : this["title"].value,
+            Director: this["director"].value
+            Genre: this["genre"].value
         };
 
         $.ajax({
